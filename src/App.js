4fb60264c413142a67efc74b75745b2fc01973c2
WanderQing <1021474307@qq.com>
2021-09-08 15:38:58 +0800

    fix: eslint --fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import MainMenu from "./components/main-menu";
-import './App.css'
-import FolderList from "./components/folder-list/FolderList";
-import {folders} from "./data/folders";
-function App() {
-  return (
-    <div className="App">
-        <MainMenu />
-        <FolderList folders={folders} />
-    </div>
-  );
-}
+import MainMenu from './components/main-menu';
+import './App.css';
+import FolderList from './components/folder-list/FolderList';
+import folders from './data/folders';
+
+
+const App = () => (
+  <div className="App">
+    <MainMenu />
+    <FolderList folders={folders} />
+  </div>
+);
 
 export default App;
